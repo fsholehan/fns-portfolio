164b04e87fc993e2c0a4f3a005e9e70548931add
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2022-11-21 09:18:25 +0700

    styling hero text

--- components/Hero/HeroLeft.js
+++ components/Hero/HeroLeft.js
@@ -1,7 +1,7 @@
 export default function HeroLeft() {
   return (
     <div className="w-full md:w-1/2 md:ml-20">
-      <h1 className="text-3xl text-center md:text-left md:text-6xl font-bold mt-10 md:mt-0">
+      <h1 className="text-3xl text-center md:text-left md:text-6xl font-bold mt-10 md:mt-0 text-transparent inline-block bg-gradient-to-b from-gray-50 to-gray-400 bg-clip-text">
         Menyatukan kepingan kode menjadi masterpiece
       </h1>
     </div>
