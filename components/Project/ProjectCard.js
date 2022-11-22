9b6399015e42029507b5652eb8d6decd730f9383
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2022-11-22 09:13:21 +0700

    add data.json

--- components/Project/ProjectCard.js
+++ components/Project/ProjectCard.js
@@ -1,6 +1,13 @@
 import Image from "next/image";
 
-export default function ProjectCard() {
+export default function ProjectCard({
+  title,
+  description,
+  image,
+  link,
+  github,
+  stacks,
+}) {
   return (
     <div
       className={`w-auto p-5 overflow-hidden bg-gradient-to-br from-rose-400 to-red-500 rounded-xl flex items-start space-x-2 h-96 even:from-violet-400 even:to-purple-500`}
@@ -8,64 +15,30 @@ export default function ProjectCard() {
       <div className="w-1/3">
         <div className="relative w-48 h-48 md:h-60 md:w-60">
           <div className="absolute -left-24 -top-8 w-full h-full rounded-full overflow-hidden border ">
-            <Image
-              layout="fill"
-              className="object-cover"
-              alt=""
-              src="https://i.pinimg.com/564x/38/85/24/388524b585e5867ab7a87ec85e61fc0f.jpg"
-            />
+            <Image layout="fill" className="object-cover" alt="" src={image} />
           </div>
         </div>
       </div>
       <div className="w-auto flex flex-col justify-around h-full">
         <div className="space-y-1">
-          <h1 className="text-base font-bold md:text-2xl">
-            Admusic - streaming songs for free
-          </h1>
-          <p className="text-xs text-white md:text-base">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-            Voluptatem, cum deleniti? Quas blanditiis, possimus saepe
-            accusantium ipsum sequi at iste quibusdam distinctio veniam delectus
-            assumenda.
-          </p>
+          <h1 className="text-base font-bold md:text-2xl">{title}</h1>
+          <p className="text-xs text-white md:text-base">{description}</p>
         </div>
         <div className="space-y-1">
           <span className="font-bold text-gray-100">Tech Stack</span>
           <div className="flex flex-wrap gap-4 items-center">
-            <div className="rounded-md overflow-hidden">
-              <Image width={25} height={25} src="/images/js.svg" alt="" />
-            </div>
-            <div className="rounded-md overflow-hidden">
-              <Image
-                width={25}
-                height={25}
-                src="/images/nextjs-logo.svg.png"
-                alt=""
-              />
-            </div>
-            <div className="rounded-md overflow-hidden">
-              <Image
-                width={25}
-                height={25}
-                src="/images/react-icon.svg.png"
-                alt=""
-              />
-            </div>
-            <div className="rounded-md overflow-hidden">
-              <Image
-                width={25}
-                height={25}
-                src="/images/tailwind-logo.svg.png"
-                alt=""
-              />
-            </div>
+            {stacks?.map((stack, i) => (
+              <div className="rounded-md overflow-hidden" key={i}>
+                <Image width={25} height={25} src={stack} alt="" />
+              </div>
+            ))}
           </div>
         </div>
         <div className="flex items-center space-x-4">
-          <a className="link-card" href="https://admusic-mu.vercel.app">
+          <a className="link-card" href={link}>
             Demo
           </a>
-          <a className="link-card" href="https://github.com">
+          <a className="link-card" href={github}>
             Github
           </a>
         </div>
--- components/Project/index.js
+++ components/Project/index.js
@@ -1,15 +1,23 @@
 import Title from "../Title";
 import ProjectCard from "./ProjectCard";
+import data from "../../service/data.json";
 
 export default function Project() {
   return (
     <div className="py-10">
       <Title title="Awakening Project" />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
-        <ProjectCard />
+        {data.map((info, i) => (
+          <ProjectCard
+            key={i}
+            title={info.title}
+            description={info.description}
+            image={info.image}
+            stacks={info.tech}
+            link={info.link}
+            github={info.github}
+          />
+        ))}
       </div>
     </div>
   );
