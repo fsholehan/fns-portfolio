15537953dd914cfe7f42b3aa6e6f023caca0fbfc
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2022-11-23 07:57:56 +0700

    add more data

--- components/Hero/HeroRight.js
+++ components/Hero/HeroRight.js
@@ -5,7 +5,7 @@ import HeroRightMenu from "./HeroRightMenu";
 /* eslint-disable react/no-unescaped-entities */
 export default function HeroRight() {
   return (
-    <div className="w-80 h-auto bg-slate-900 text-gray-50 p-5 rounded-xl mx-auto">
+    <div className="w-80 h-auto bg-slate-900 text-gray-50 p-5 rounded-xl mx-auto shadow-xl shadow-gray-800">
       <div>
         <div className="flex space-x-1 items-center">
           <span className="text-red-500 font-bold">"menu"</span>
