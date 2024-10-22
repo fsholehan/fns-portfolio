365dc6dd35eec1218956944883c722e0742f1a11
Fuad Sholehan <68955196+fsholehan@users.noreply.github.com>
2024-10-23 06:31:22 +0700

    add new content

--- next.config.js
+++ next.config.js
@@ -2,7 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ["i.pinimg.com", "raw.githubusercontent.com"],
+    domains: [
+      "i.pinimg.com",
+      "raw.githubusercontent.com",
+      "play-lh.googleusercontent.com",
+    ],
   },
 };
 
